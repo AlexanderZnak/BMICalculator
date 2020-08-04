6213e1bad17d1a7e7a30973772b0c268ef8f703a
AlexanderZnak <frolovruslan1990@gmail.com>
2020-08-04 15:43:30 +0300

    Remade

--- src/test/java/NumbersToString.java
+++ src/test/java/NumbersToString.java
@@ -11,19 +11,19 @@ public class NumbersToString {
                 return temp;
             }
             else {
-                return " " + temp + " ";
+                return " " + temp;
             }
     }
 
     public static String getCompare(String numb, int begin, int end) {
         if (Integer.parseInt(numb.substring(begin, end)) == 1) {
-            return " тысяча ";
+            return " тысяча";
         }
         else if (Integer.parseInt(numb.substring(begin, end)) == 2 | Integer.parseInt(numb.substring(begin, end)) == 3 | Integer.parseInt(numb.substring(begin, end)) == 4) {
-            return " тысячи ";
+            return " тысячи";
         }
         else
-            return " тысяч ";
+            return " тысяч";
     }
 
     public static String conversion(int a) {
@@ -39,62 +39,62 @@ public class NumbersToString {
             return "Программа работает от 1 до 999999";
         }
         else if (numb.length() == 1) {
-            return checkNumber(one, numb,0, 1);
+            return (checkNumber(one, numb,0, 1)).trim();
         }
         else if (numb.length() == 2) {
             if (Integer.parseInt(numb.substring(0, 1)) == 1) {
-                return checkNumber(one, numb, 0, 2);
+                return (checkNumber(one, numb, 0, 2)).trim();
             }
             else {
-                return (checkNumber(ten, numb, 0, 1) + " " + checkNumber(one, numb, 1, 2)).trim();
+                return (checkNumber(ten, numb, 0, 1) + checkNumber(one, numb, 1, 2)).trim();
             }
         }
         else if (numb.length() == 3) {
             if (Integer.parseInt(numb.substring(1, 2)) == 1) {
-                return checkNumber(hundred, numb, 0, 1) + " " + checkNumber(one, numb, 1, 3);
+                return (checkNumber(hundred, numb, 0, 1) + checkNumber(one, numb, 1, 3)).trim();
             } else {
-                return (checkNumber(hundred, numb, 0, 1) + " " + checkNumber(ten, numb, 1, 2) + " " + checkNumber(one, numb, 2, 3)).trim();
+                return (checkNumber(hundred, numb, 0, 1) + checkNumber(ten, numb, 1, 2) + checkNumber(one, numb, 2, 3)).trim();
             }
         }
         else if (numb.length() == 4) {
             if (Integer.parseInt(numb.substring(2, 3)) == 1) {
-                return (checkNumber(two, numb, 0, 1) + getCompare(numb,0,1) + checkNumber(hundred,numb, 1, 2) + " " + checkNumber(one, numb, 2, 4)).trim();
+                return (checkNumber(two, numb, 0, 1) + getCompare(numb,0,1) + checkNumber(hundred,numb, 1, 2) + checkNumber(one, numb, 2, 4)).trim();
             }
-            return (checkNumber(two, numb, 0, 1) + getCompare(numb,0,1) + checkNumber(hundred,numb, 1, 2) + " " + checkNumber(ten, numb, 2, 3) + " " + checkNumber(one, numb, 3, 4)).trim();
+            return (checkNumber(two, numb, 0, 1) + getCompare(numb,0,1) + checkNumber(hundred,numb, 1, 2) + checkNumber(ten, numb, 2, 3) + checkNumber(one, numb, 3, 4)).trim();
         }
         else if (numb.length() == 5) {
             if (Integer.parseInt(numb.substring(0, 1)) == 1) {
                 if (Integer.parseInt(numb.substring(3, 4)) == 1) {
-                    return (checkNumber(one, numb, 0, 2) + getCompare(numb, 0, 2) + checkNumber(hundred, numb, 2, 3) + " " + checkNumber(one, numb, 3, 5)).trim();
+                    return (checkNumber(one, numb, 0, 2) + getCompare(numb, 0, 2) + checkNumber(hundred, numb, 2, 3) + checkNumber(one, numb, 3, 5)).trim();
                 }
                 else {
-                    return (checkNumber(one, numb, 0, 2) + getCompare(numb, 0, 2) + checkNumber(hundred, numb, 2, 3) + " " + checkNumber(ten, numb, 3, 4) + " " + checkNumber(one, numb, 4, 5)).trim();
+                    return (checkNumber(one, numb, 0, 2) + getCompare(numb, 0, 2) + checkNumber(hundred, numb, 2, 3) + checkNumber(ten, numb, 3, 4) + checkNumber(one, numb, 4, 5)).trim();
                 }
             }
             else {
                 if (Integer.parseInt(numb.substring(3, 4)) == 1) {
-                    return (checkNumber(ten, numb, 0, 1) + " " + checkNumber(two, numb, 1 , 2) + getCompare(numb, 1, 2) + checkNumber(hundred, numb, 2, 3) + " " + checkNumber(one, numb, 3, 5)).trim();
+                    return (checkNumber(ten, numb, 0, 1) + checkNumber(two, numb, 1 , 2) + getCompare(numb, 1, 2) + checkNumber(hundred, numb, 2, 3) + checkNumber(one, numb, 3, 5)).trim();
                 }
                 else {
-                    return (checkNumber(ten, numb, 0, 1) + " " + checkNumber(two, numb, 1 , 2) + getCompare(numb, 1, 2) + checkNumber(hundred, numb, 2, 3) + " " + checkNumber(ten, numb, 3, 4) + " " + checkNumber(one, numb, 4, 5)).trim();
+                    return (checkNumber(ten, numb, 0, 1) + checkNumber(two, numb, 1 , 2) + getCompare(numb, 1, 2) + checkNumber(hundred, numb, 2, 3) + checkNumber(ten, numb, 3, 4) + checkNumber(one, numb, 4, 5)).trim();
                 }
             }
         }
         else if (numb.length() == 6) {
             if (Integer.parseInt(numb.substring(1, 2)) == 1) {
                 if (Integer.parseInt(numb.substring(4, 5)) == 1) {
-                    return (checkNumber(hundred, numb, 0, 1) + " " + checkNumber(one, numb, 1, 3) + getCompare(numb, 1, 3) + checkNumber(hundred, numb, 3, 4) + " " + checkNumber(one, numb, 4,6)).trim();
+                    return (checkNumber(hundred, numb, 0, 1)  + checkNumber(one, numb, 1, 3) + getCompare(numb, 1, 3) + checkNumber(hundred, numb, 3, 4) + checkNumber(one, numb, 4,6)).trim();
                 }
                 else {
-                    return (checkNumber(hundred, numb, 0, 1) + " " + checkNumber(one, numb, 1, 3) + getCompare(numb, 1, 3) + checkNumber(hundred, numb, 3, 4) + " " + checkNumber(ten, numb, 4,5) + " " + checkNumber(one, numb, 5, 6)).trim();
+                    return (checkNumber(hundred, numb, 0, 1) + checkNumber(one, numb, 1, 3) + getCompare(numb, 1, 3) + checkNumber(hundred, numb, 3, 4) + checkNumber(ten, numb, 4,5) + checkNumber(one, numb, 5, 6)).trim();
                 }
             }
             else {
                 if (Integer.parseInt(numb.substring(4, 5)) == 1) {
-                    return (checkNumber(hundred, numb, 0, 1) + " " + checkNumber(ten, numb, 1, 2) + " " + checkNumber(two, numb, 2, 3) + getCompare(numb, 2, 3) + checkNumber(hundred, numb, 3, 4) + " " + checkNumber(one, numb, 4,6)).trim();
+                    return (checkNumber(hundred, numb, 0, 1) + checkNumber(ten, numb, 1, 2) + checkNumber(two, numb, 2, 3) + getCompare(numb, 2, 3) + checkNumber(hundred, numb, 3, 4) + checkNumber(one, numb, 4,6)).trim();
                 }
                 else {
-                    return (checkNumber(hundred, numb, 0, 1) + " " + checkNumber(ten, numb, 1, 2) + " " + checkNumber(two, numb, 2, 3) + getCompare(numb, 2, 3) + checkNumber(hundred, numb, 3, 4) + " " + checkNumber(ten, numb, 4,5) + " " + checkNumber(one, numb, 5, 6)).trim();
+                    return (checkNumber(hundred, numb, 0, 1) + checkNumber(ten, numb, 1, 2) + checkNumber(two, numb, 2, 3) + getCompare(numb, 2, 3) + checkNumber(hundred, numb, 3, 4) + checkNumber(ten, numb, 4,5) + checkNumber(one, numb, 5, 6)).trim();
                 }
             }
         }
